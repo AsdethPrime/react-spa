95215a86e7fe080cf8484cec143f7b47949b1ade
asdethprime <asdethprime@gmail.com>
2020-01-29 01:55:07 +0000

    spa created

---
+++ src/Contact.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Got Questions</h2>
+                <p>The easiest thing to do will be kill yourself and all your problems go away</p>                
+                <p>If that's too sensitive, bitch please... grow up!!</p> 
+                <a href='http://google.com'>Google</a>               
+            </div>
+        )
+    }
+}
---
+++ src/Stuff.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class Stuff extends Component {
+    render() {
+        return (
+            <div>
+                <h2>STUFF</h2>
+                <p> First para from the stuff page. Nothing here is meaningful just random crap</p> 
+                <p> Second para of random crap coz fuck why not!!</p>  
+                <ol>
+                    <li>A B C</li>
+                    <li>D E F</li>
+                    <li>G H I</li>
+                    <li>J K L</li>
+                </ol>            
+            </div>
+        )
+    }
+}
